c9915ae8931ee3271811f4934179ca38c2c48d8d
Dario Nicosia <darionicosia@hotmail.com>
2020-06-29 09:29:42 +0100

    modified header

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-card max-width="700" flat class="px-2 header-card">
+  <div max-width="700" flat class="px-2 header-card">
     <h1 class="text-h3 text-sm-h1 mt-5 mb-2 header indigo--text text--darken-3">gettymeta</h1>
 
     <slot name="subtitle"></slot>
     <slot name="input"></slot>
-  </v-card>
+  </div>
 </template>
 
 <script>
